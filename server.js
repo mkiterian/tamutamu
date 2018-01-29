ea8bfa9cdd2a551d88f0bf51e96b694bdf0e3460
mkiterian <hmukite@gmail.com>
2018-01-29 12:24:37 +0300

    Create a route to post a recipe

--- server.js
+++ server.js
@@ -10,13 +10,39 @@ require('./app/models/User');
 mongoose.Promise = global.Promise;
 mongoose.connect(config.mongoURI);
 
+const Recipe = mongoose.model('Recipe');
+const User = mongoose.model('User');
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.post('/recipes', (req, res) => {
+  const {
+    name,
+    description,
+    imageUrl,
+    ingredients,
+    directions } = req.body;
+
+  const newRecipe = Recipe({
+    name,
+    description,
+    imageUrl,
+    ingredients,
+    directions
+  });
+
+  newRecipe.save().then(doc => {
+    res.status(201).send(doc);
+  }).catch(err => {
+    res.status(400).send(err);
+  });
+});
+
 const PORT = process.env.PORT || config.PORT;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT} ...`);
 });
 
