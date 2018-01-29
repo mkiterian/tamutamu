23d631295c2edf78b4f943ed52fec766e3f38c47
mkiterian <hmukite@gmail.com>
2018-01-29 14:50:27 +0300

    fix env variables

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
-} else if (process.env.NODE_ENV === 'test') {
+} else if (env.NODE_ENV === 'test') {
   module.exports = require('./test');
 } else {
   module.exports = require('./dev');
